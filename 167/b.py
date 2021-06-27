8e917b3b812615d0bc1a09870b9834eb835d6000
Noah Chen <noahchen.u@gmail.com>
2021-06-26 21:13:05 -0400

    extra problems

--- 167/b.py
+++ 167/b.py
@@ -1,1 +1,10 @@
-A, B, C, K = map(int, input().split()
+A, B, C, K = map(int, input().split())
+T = 0
+if A >= K:
+    T += K
+if A < K:
+    if B >= K - A:
+        T += A
+    if B < K - A:
+        T += 2*A + B - K
+print(T)
---
+++ 167/c.py
@@ -0,0 +1,4 @@
+N, M, X = map(int, input().split())
+for i in N:
+    for j in M:
+        
